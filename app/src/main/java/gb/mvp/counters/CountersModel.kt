8422d58fa9565bc3c3193f37821bc8c779055153
Vitaliy Grakhov <v.a.grahov@ya.ru>
2021-09-23 12:29:00 +0500

    подчистка

--- app/src/main/java/gb/mvp/counters/CountersModel.kt
+++
@@ -1,6 +0,0 @@
-package gb.mvp.counters
-
-class CountersModel(private val counters: MutableList<Int> = mutableListOf(0, 0, 0)) {
-
-    fun incrementCounter(counterId: Int) = ++counters[counterId]
-}
